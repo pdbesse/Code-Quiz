f4e7244b81fdff2ec829c9d64b2f44b2084666fe
Phillip <phillip.besse@gmail.com>
2022-06-26 00:17:08 -0400

    changed from createElement to having structure in index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
-var body = document.body;
-var h1El = document.createElement("h1");
-var quizContainEl = document.createElement("div");
-var startButton = document.createElement("button")
-var quest = document.createElement("h2");
-var answersContainEl = document.createElement("div");
-var answersList = document.createElement("ul");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
+var h1El = document.querySelector("#h1");
+var quizContainer = document.querySelector(".quizcontainer");
+var startButton = document.querySelector("#startbutton")
+var quest = document.querySelector(".question");
+var answersList = document.querySelector(".answerslist");
+var a1 = document.querySelector("#a1");
+var a2 = document.querySelector("#a2");
+var a3 = document.querySelector("#a3");
+var a4 = document.querySelector("#a4");
 
 var quizQuestions = {
     q1: "JavaScript is a ________-side programming language.",
@@ -17,61 +15,49 @@ var quizQuestions = {
     q4: "Which of the following is a correct 'if' statement to execute certain code if 'x' is equal to 2?",
 }
 
-var quizAnswers = {
-    q1a1: "client",
-    q1a2: "server",
-    q1a3: "both", // correct
-    q1a4: "none of the above",
-    q2a1: "alertBox(:'Hello User!')",
-    q2a2: "alert(Hello User!)",
-    q2a3: "confirm('Hello User!')",
-    q2a4: "alert('Hello User!')", // correct
-    q3a1: "{}", // correct
-    q3a2: "()",
-    q3a3: "[]",
-    q3a4: "//",
-    q4a1: "if (x=2",
-    q4a2: "if (x==2)", // correct
-    q4a3: "if (!x=2)",
-    q4a4: "if (x=2_",
+var q1Answers = {
+    q1a1: "client", isCorrect: false,
+    q1a2: "server", isCorrect: false,
+    q1a3: "both", isCorrect: true,
+    q1a4: "none of the above", isCorrect: false,}
+
+var q2Answers = {
+    q2a1: "alertBox(:'Hello User!')", isCorrect: false,
+    q2a2: "alert(Hello User!)", isCorrect: false,
+    q2a3: "confirm('Hello User!')", isCorrect: false,
+    q2a4: "alert('Hello User!')", isCorrect: true,}
+    
+var q3Answers = {
+    q3a1: "{}", isCorrect: true,
+    q3a2: "()", isCorrect: false,
+    q3a3: "[]", isCorrect: false,
+    q3a4: "//", isCorrect: false,}
+
+var q4Answers = {
+    q4a1: "if (x=2)", isCorrect: false,
+    q4a2: "if (x==2)", isCorrect: true,
+    q4a3: "if (!x=2)", isCorrect: false,
+    q4a4: "if (x=2)", isCorrect: false,
 }
 
+var trueFalse = {
+    true: true,
+    false: false,
+}
 
-body.appendChild(h1El);
-body.appendChild(quizContainEl);
-quizContainEl.appendChild(quest);
-quizContainEl.appendChild(answersContainEl);
-answersContainEl.appendChild(answersList);
-answersList.appendChild(li1);
-answersList.appendChild(li2);
-answersList.appendChild(li3);
-answersList.appendChild(li4);
-quizContainEl.appendChild(startButton);
+var currentQ = 0;
+var prevQ = quizQuestions.length - 1;
 
-h1El.textContent = "Code Quiz!";
-startButton.textContent = "Start Quiz";
+startButton.addEventListener("click", function Quiz(){
+    quest.textContent = quizQuestions.q1;
+    a1.textContent = quizAnswers.q1a1;
+    a2.textContent = quizAnswers.q1a2;
+    a3.textContent = quizAnswers.q1a3;
+    a4.textContent = quizAnswers.q1a4;
+   
 
-console.log("this");
-
-body.setAttribute("style", "background-color: #3687d9; font-family: Verdana, Geneva, Tahoma, sans-serif;");
-quizContainEl.setAttribute("style", "display: flex; align-items: center; justify-content: center;");
-quest.setAttribute("style", "display: flex; align-items: center; justify-content; center;");
-answersContainEl.setAttribute("style", "display: flex; align-items: center; justify-content: center;");
-answersList.setAttribute("style", "list-style:none; display: flex;");
-li1.setAttribute("style", "display: flex; align-items: lef;");
-li2.setAttribute("style", "display: flex; align-items: left;");
-li3.setAttribute("style", "display: flex; align-items: left;");
-li4.setAttribute("style", "display: flex; align-items: left;");
-/* startButton.setAttribute("id", "start") */
-
-startButton.addEventListener("click", function(){
-    quest.textContent = quizQuestions.q1
-    li1.textContent = quizAnswers.q1a1
-    li2.textContent = quizAnswers.q1a2
-    li3.textContent = quizAnswers.q1a3
-    li4.textContent = quizAnswers.q1a4
-
-
+    
 })
 
-console.log(window);
+
+
