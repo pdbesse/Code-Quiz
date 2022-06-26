1d4cd3f89066b4dcd4f42541092589d2035fa814
Phillip <phillip.besse@gmail.com>
2022-06-26 10:25:34 -0400

    added 60 sec timer on start button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ var a1 = document.querySelector("#a1");
 var a2 = document.querySelector("#a2");
 var a3 = document.querySelector("#a3");
 var a4 = document.querySelector("#a4");
+var timer = document.querySelector(".timer");
 
 var quizQuestions = {
     q1: "JavaScript is a ________-side programming language.",
@@ -15,7 +16,26 @@ var quizQuestions = {
     q4: "Which of the following is a correct 'if' statement to execute certain code if 'x' is equal to 2?",
 }
 
-var q1Answers = {
+var quizAnswers = {
+    q1a1: "client",
+    q1a2: "server",
+    q1a3: "both", // correct
+    q1a4: "none of the above",
+    q2a1: "alertBox(:'Hello User!')",
+    q2a2: "alert(Hello User!)",
+    q2a3: "confirm('Hello User!')",
+    q2a4: "alert('Hello User!')", // correct
+    q3a1: "{}", // correct
+    q3a2: "()",
+    q3a3: "[]",
+    q3a4: "//",
+    q4a1: "if (x=2",
+    q4a2: "if (x==2)", // correct
+    q4a3: "if (!x=2)",
+    q4a4: "if (x=2_",
+}
+
+/* var q1Answers = {
     q1a1: "client", isCorrect: false,
     q1a2: "server", isCorrect: false,
     q1a3: "both", isCorrect: true,
@@ -39,6 +59,7 @@ var q4Answers = {
     q4a3: "if (!x=2)", isCorrect: false,
     q4a4: "if (x=2)", isCorrect: false,
 }
+ */
 
 var trueFalse = {
     true: true,
@@ -54,10 +75,39 @@ startButton.addEventListener("click", function Quiz(){
     a2.textContent = quizAnswers.q1a2;
     a3.textContent = quizAnswers.q1a3;
     a4.textContent = quizAnswers.q1a4;
-   
+/*     a1.addEventListener("click", function next(){
 
+    });
+    a2.addEventListener("click", function next(){
+
+    });
+    a3.addEventListener("click", function next(){
+
+    });
+    a4.addEventListener("click", function next(){
+
+    }); */
+   
+    timerStart()
     
 })
 
+var secondsLeft = 60;
+function timerStart(){
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timer.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000)
+    console.log(secondsLeft);
+}
+
+/* function next(){
+
+}
+ */
 
 
